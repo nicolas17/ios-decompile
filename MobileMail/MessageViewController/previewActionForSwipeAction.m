6e6b20922a59dd5d4908ddb379f55e1af0d373d5
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2016-02-15 01:24:21 -0300

    Replace stack variable mess by code initializing a block_literal struct.

--- MobileMail/MessageViewController/previewActionForSwipeAction.m
+++ MobileMail/MessageViewController/previewActionForSwipeAction.m
@@ -7,6 +7,23 @@
 
 #include <UIKit/UIKit.h>
 
+struct Block_literal_1 {
+    void *isa; // initialized to &_NSConcreteStackBlock or &_NSConcreteGlobalBlock
+    int flags;
+    int reserved;
+    void (*invoke)(void *, ...);
+    struct Block_descriptor_1 {
+        unsigned long int reserved;     // NULL
+        unsigned long int size;         // sizeof(struct Block_literal_1)
+        // optional helper functions
+        void (*copy_helper)(void *dst, void *src);     // IFF (1<<25)
+        void (*dispose_helper)(void *src);             // IFF (1<<25)
+        // required ABI.2010.3.16
+        const char *signature;                         // IFF (1<<30)
+    } *descriptor;
+    // imported variables
+};
+
 -(UIPreviewAction*)_previewActionForSwipeAction: (unsigned long long)action
                                     withMessage: (id)message
                                        forSwipe: (BOOL)swipe
@@ -41,18 +58,18 @@
     //0x10009bd54
     x23 = [[NSBundle mainBundle] localizedStringForKey: @"PREVIEW_SWIPE_ARCHIVE" value:"" table:"Main-OrbHW"];
 
-    // This is initializing a block_literal struct
     w24 = 0;
-    var_110 = _NSConcreteStackBlock; //64-bit
-    var_118 = w28; // 32-bit
-    var_11c = 0x00000000; // 32-bit, wzr
-    var_120 = 0x10009c82c; // 64-bit
-    var_128 = 0x1001fe1e0; // 64-bit
-    var_148 = swipe; // 8-bit
-    var_130 = self; // both 64-bit; these come from a STP that I'm not sure about
-    var_138 = message;
-    var_140 = x25;
-    x27 = &var_110;
+    Block_literal_1 myBlock;
+    myBlock.isa = &_NSConcreteStackBlock;
+    myBlock.flags = w28;
+    myBlock.reserved = 0;
+    myBlock.invoke = 0x10009c82c;
+    myBlock.descriptor = 0x1001fe1e0;
+    myBlock.imported1 = self;
+    myBlock.imported2 = message;
+    myBlock.imported3 = x25;
+    myBlock.imported4 = swipe; // 8-bit
+    x27 = &myBlock;
     w26 = 0x2; // orr w26, wzr, #0x2
     goto jump_A_124;
 
