389fd859dc3972ff143194ae72bc63d1caf56d4e
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2016-02-21 00:01:24 -0300

    Add script to generate control flow graphs from decompiled code.
    It expects some pretty specific C/ObjC syntax, but I guess I can make it
    more flexible in the future.

---
+++ pseudocode-to-cfg.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+
+# Convert decompiled code into a control flow graph.
+# Copyright (c) 2016 Nicolás Alvarez <nicolas.alvarez@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import re
+
+def skip_past(lineit, prefix):
+    while True:
+        l = next(lineit)
+        if l.startswith(prefix):
+            break
+
+
+class BasicBlock:
+    def __init__(self, label):
+        self.content = ''
+        self.label = label
+        self.color = None
+        self.is_last = False
+
+filename = sys.argv[1]
+
+blocks = []
+blocks_by_label = {}
+links=[]
+
+def start_new_block(label=None):
+    if label is None:
+        label = "_block{:02x}".format(len(blocks))
+    block = BasicBlock(label)
+    blocks.append(block)
+    blocks_by_label[label] = block
+    return block
+
+STATE_IN_BLOCK = object()
+STATE_OUTSIDE = object()
+STATE_IN_SWITCH = object()
+
+state = None
+
+with open(filename, "r") as fp:
+    lineit = iter(fp)
+    skip_past(lineit, "@implementation ")
+    skip_past(lineit, "-(")
+    skip_past(lineit, "{")
+
+    current_block = start_new_block("init")
+    state = STATE_IN_BLOCK
+
+    for line in lineit:
+        m = re.match(r'\s*@end\s*$', line)
+        if m:
+            assert state in (STATE_IN_BLOCK, STATE_OUTSIDE)
+            state = None
+            break
+
+        m = re.match(r'([A-Za-z_][A-Za-z0-9_]*)\s*:', line)
+        if m:
+            assert state in (STATE_IN_BLOCK, STATE_OUTSIDE)
+            state = STATE_IN_BLOCK
+            print("Found label {}".format(m.group(1)))
+            current_block = start_new_block(m.group(1))
+            current_block.content += line
+            continue
+
+        m=re.match(r'    goto ([A-Za-z_][A-Za-z0-9_]*);$', line)
+        if m:
+            assert state == STATE_IN_BLOCK
+            state = STATE_OUTSIDE
+            print("Found goto {}".format(m.group(1)))
+            current_block.content += line
+            links.append((current_block.label, m.group(1), "goto"))
+            continue
+
+        m=re.match(r'    if\b.*\bgoto ([A-Za-z_][A-Za-z0-9_]*);$', line)
+        if m:
+            assert state == STATE_IN_BLOCK
+            print("Found conditional goto {}".format(m.group(1)))
+            current_block.content += line
+            new_block = start_new_block()
+            links.append((current_block.label, m.group(1), "true"))
+            links.append((current_block.label, new_block.label, "false"))
+            current_block = new_block
+            continue
+
+        m=re.match(r'    switch\s*\(\s*(.*)\s*\)\s*\{', line)
+        if m:
+            assert state == STATE_IN_BLOCK
+            state = STATE_IN_SWITCH
+            print("Found switch! Condition is {}".format(m.group(1)))
+            current_block.content += line
+            continue
+
+        m=re.match(r'\s+case\s+(\d+):\s*goto\s+([A-Za-z_][A-Za-z0-9_]*);', line)
+        if m:
+            assert state == STATE_IN_SWITCH
+            print("Found case! If {} goto {}".format(m.group(1), m.group(2)))
+            links.append((current_block.label, m.group(2), m.group(1)))
+            current_block.content += line
+            continue
+
+        if state == STATE_IN_SWITCH:
+            if line.startswith('    }'):
+                state = STATE_IN_BLOCK
+
+        if state == STATE_IN_BLOCK:
+            current_block.content += line
+
+
+blocks[-1].is_last = True
+
+def graphviz_escape(text):
+    text = text.replace('\\', '\\\\')
+    text = text.replace('"', '\\"')
+    text = text.replace('\n', '\\l')
+    return text
+
+for block in blocks:
+    block.content = "["+block.label+"]\n"+block.content.lstrip('\n').rstrip()+'\n'
+
+for i, block in enumerate(blocks):
+    if not any(block.label == link[0] for link in links) and not block.is_last:
+        links.append((block.label, blocks[i+1].label, "fallthru"))
+
+with open("cfg.dot", "w") as f:
+    f.write("""
+    digraph {
+        node[shape=rectangle fontname=monospace];
+    """)
+    for block in blocks:
+        f.write('    "{}" [label="{}"'.format(block.label, graphviz_escape(block.content)))
+        if block.color is not None:
+            f.write(' style=filled fillcolor="{}"'.format(graphviz_escape(block.color)))
+        f.write(']\n')
+    for bfrom, bto, linklabel in links:
+        f.write('    "{}" -> "{}" [taillabel="{}"]\n'.format(
+            graphviz_escape(bfrom),
+            graphviz_escape(bto),
+            graphviz_escape(linklabel)
+        ))
+    f.write("}\n")
